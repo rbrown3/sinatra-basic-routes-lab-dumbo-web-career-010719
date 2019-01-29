835f872e442b82a6c00c89a8d18eaf51c8495f40
rbrown3 <rosannabrown@outlook.com>
2019-01-29 14:37:13 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+      "My name is Rosanna Brown."
+    end
+
+    get '/hometown' do
+       "My hometown is New York City"
+    end
+
+    get '/favorite-song' do
+    "My favorite song is I didn't know my own strength! by Whitney Houston"
+    end
 end
